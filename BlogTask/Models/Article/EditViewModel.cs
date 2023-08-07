6033e9689068cec7c656ea8159f43c65e8b3e9c9
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2023-08-07 20:45:37 +0300

    Добавлена страница редактирования статьи

---
+++ BlogTask/Models/Article/EditViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogTask.Models.Article
+{
+    public class EditViewModel
+    {
+        [Required(ErrorMessage = "Поле Заголовок обязательно для заполнения")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Заголовок", Prompt = "Введите заголовок статьи")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Поле Контент обязательно для заполнения")]
+        [DataType(DataType.Text)]
+        [Display(Name = "Контент", Prompt = "Введите текст статьи")]
+        public string Text { get; set; }
+    }
+}
